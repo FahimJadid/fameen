ca4af0f1b8d7d81694967726cda5964973dfea59
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2022-08-23 05:25:32 +0600

    02-Listing-Products

---
+++ client/src/data.js
@@ -0,0 +1,58 @@
+const data = {
+    products: [
+        {
+            name: 'Pharmacy Jersey',
+            slug: 'pharmacy-jersey',
+            category:'Jerseys',
+            image:'/images/1.png', //679-829
+            price: 400,
+            countInStock: 10,
+            brand: 'JnU',
+            rating: 4.8,
+            numReviews: 10,
+            description: 'Breathable High Quality Jersey'
+        },
+
+        {
+            name: 'Pharmacy Shirt',
+            slug: 'pharmacy-shirt',
+            category:'Shirts',
+            image:'/images/2.png',
+            price: 800,
+            countInStock: 20,
+            brand: 'JnU',
+            rating: 4.7,
+            numReviews: 13,
+            description: 'High Quality Cotton Shirts'
+        },
+
+        {
+            name: 'Pharmacy T-shirt',
+            slug: 'pharmacy-t-shirt',
+            category:'Jerseys',
+            image:'/images/3.png',
+            price: 300,
+            countInStock: 19,
+            brand: 'JnU',
+            rating: 4.9,
+            numReviews: 11,
+            description: 'Cotton Fabrics 160 GSM'
+        },
+
+        {
+            name: 'RAW NATION',
+            slug: 'raw-nation',
+            category:'T-shirts',
+            image:'/images/4.png',
+            price: 599,
+            countInStock: 10,
+            brand: 'RAW NATION',
+            rating: 4.5,
+            numReviews: 18,
+            description: 'Cotton 95% and Polyester 5%'
+        },
+    ],
+};
+
+
+export default data;
