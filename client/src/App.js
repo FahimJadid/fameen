86c727db6c0f14f6df67ceca1d2aa9a76edc1c9e
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2022-08-26 05:26:22 +0600

    Using React Bootstrap for styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,37 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-      <header>
-        <Link to="/">Fameen</Link>
-      </header>
-      <main>
-      <Routes>
-        <Route path='/product/:slug' element={<ProductScreen />}/>
-        <Route path='/' element={<HomeScreen />} />
-      </Routes>
-      </main>
-    </div>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Fameen</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
+
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+
+        <footer>
+          <div className="text-center">All Rights Reserved</div>
+        </footer>
+      </div>
     </BrowserRouter>
   );
 }
